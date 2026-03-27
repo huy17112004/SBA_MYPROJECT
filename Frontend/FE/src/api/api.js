9c4d994c8dc37b0a0923899f9246de87eeaa7a2d
Đinh Thiên Huy <huy171104@gmail.com>
2026-03-27 11:55:43 +0700

    Update BaseURL FE

--- Frontend/FE/src/api/api.js
+++ Frontend/FE/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  // baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://sba-myproject.onrender.com/api'
 });
 
 // Add a response interceptor to handle the ApiResponse wrapper
